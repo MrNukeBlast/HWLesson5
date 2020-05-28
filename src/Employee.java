18091742070f6fb28bc3d0a7abcf048ba0b0bc23
MrNukeBlast <bnikolayev993@gmail.com>
2020-05-28 07:12:52 -0700

    HW5

--- src/Employee.java
+++ src/Employee.java
@@ -7,9 +7,7 @@ public class Employee {
     String tel;
     String salary;
     int age;
-
-
-
+//Конструктор для создания сотрудника
     public Employee(String name, String mansion, String email, String tel, String salary, int age) {
     this.name = name;
     this.mansion = mansion;
@@ -18,10 +16,12 @@ public class Employee {
     this.salary = salary;
     this.age = age;
     }
-
+//Метод вывода инвормации о сотруднике на єкран
     public void employeeInfo(){
-        System.out.println("Сотрудник "+ name + "eMail " + "телефон: "+ tel + "зарплата " + salary + "возраст " + age);
+        System.out.println("Сотрудник "+ name + " eMail " + email + " телефон: "+ tel + " зарплата " + salary + " возраст " + age);
     }
-
-
+//Вытащить возраст с сотрудника чтоб закинуть в цыкл
+    public int empAge(){
+        return age;
+    }
 }
